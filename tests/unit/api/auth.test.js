7977bb281135ec7d4b0c128ba53070be2d4d5295
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-09 14:16:41 +0200

    Added --coverage to test jest

--- tests/unit/api/auth.test.js
+++ tests/unit/api/auth.test.js
@@ -1,59 +1,73 @@
 import { login } from "../../../src/js/api/auth/login";
+import { logout } from "../../../src/js/api/auth/logout";
 import * as storage from "../../../src/js/storage"; // Import the entire storage module
 
 // Mock the storage module
 jest.mock("../../../src/js/storage", () => ({
     save: jest.fn(),
     load: jest.fn(),
+    remove: jest.fn(),
 }));
-
-describe("Login function", () => {
+describe("Auth functions", () => {
     beforeEach(() => {
         jest.clearAllMocks(); // Clear all mock function calls before each test
     });
+    describe("Login function", () => {
+        test("should store token in storage when valid credentials are provided", async () => {
+            console.log("Starting the login test...");
 
-    test("should store token in storage when valid credentials are provided", async () => {
-        console.log("Starting the login test...");
+            // Mock the fetch API to simulate a successful login response
+            global.fetch = jest.fn(() =>
+                Promise.resolve({
+                    ok: true,
+                    json: () =>
+                        Promise.resolve({
+                            accessToken: "mock-token",
+                            name: "test-user",
+                        }),
+                })
+            );
 
-        // Mock the fetch API to simulate a successful login response
-        global.fetch = jest.fn(() =>
-            Promise.resolve({
-                ok: true,
-                json: () =>
-                    Promise.resolve({
-                        accessToken: "mock-token",
-                        name: "test-user",
+            // Mock to simulate no token initially in the storage
+            storage.load.mockReturnValueOnce(null);
+
+            console.log("Calling login function...");
+
+            // Call the login function with valid credentials
+            await login("test@noroff.no", "password12345");
+
+            console.log("Login function called, checking expectations...");
+
+            // Check that the token was stored correctly
+            expect(storage.save).toHaveBeenCalledWith("token", "mock-token");
+
+            // Verify that the fetch function was called with correct arguments
+            expect(fetch).toHaveBeenCalledWith(
+                "https://nf-api.onrender.com/api/v1/social/auth/login",
+                expect.objectContaining({
+                    method: "post",
+                    body: JSON.stringify({
+                        email: "test@noroff.no",
+                        password: "password12345",
                     }),
-            })
-        );
+                    headers: expect.any(Object),
+                })
+            );
 
-        // Mock to simulate no token initially in the storage
-        storage.load.mockReturnValueOnce(null);
+            console.log("Login test finished!");
+        });
+    });
 
-        console.log("Calling login function...");
+    describe("Log out function", () => {
+        test("should clear the token from browser storage", () => {
+            console.log("Starting the logout test..");
+            logout();
+            console.log("Call the logout function");
+            expect(storage.remove).toHaveBeenCalledWith("token");
+            expect(storage.remove).toHaveBeenCalledWith("profile");
 
-        // Call the login function with valid credentials
-        await login("test@noroff.no", "password12345");
-
-        console.log("Login function called, checking expectations...");
-
-        // Check that the token was stored correctly
-        expect(storage.save).toHaveBeenCalledWith("token", "mock-token");
-
-        // Verify that the fetch function was called with correct arguments
-        expect(fetch).toHaveBeenCalledWith(
-            "https://nf-api.onrender.com/api/v1/social/auth/login",
-            expect.objectContaining({
-                method: "post",
-                body: JSON.stringify({
-                    email: "test@noroff.no",
-                    password: "password12345",
-                }),
-                headers: expect.any(Object),
-            })
-        );
-
-        console.log("Test finished!");
+            console.log("Logout test finished!");
+        });
     });
 });
 
