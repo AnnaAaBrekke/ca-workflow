8033bd98bea7525e7d0c8e00ba8ac00dfbeda912
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-09 16:27:46 +0200

    Updated folder structure and Cypress config for E2E tests

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+    e2e: {
+        // Updated path to match my own folder system
+        specPattern: "tests/e2e/cypress/specs/**/*.cy.js",
+        supportFile: "tests/e2e/cypress/support/commands.js",
+        fixturesFolder: "tests/e2e/cypress/fixtures",
+        setupNodeEvents(/* on, config */) {
+            // implement node event listeners here
+        },
+    },
+});
