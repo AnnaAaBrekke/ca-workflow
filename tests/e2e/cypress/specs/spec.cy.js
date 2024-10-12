dd0215546b717458bb5c41b0a075adade7fe3068
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-12 16:17:47 +0200

    e2e testing cleaned up

--- tests/e2e/cypress/specs/spec.cy.js
+++ tests/e2e/cypress/specs/spec.cy.js
@@ -3,31 +3,3 @@
 // //         cy.visit("https://example.cypress.io");
 // //     });
 // // });
-
-// it("should log out the user with the logout button", () => {
-//   // Simulate user login by setting the token in local storage
-//   cy.window().then((win) => {
-//       win.localStorage.setItem("token", "fakeAccessToken");
-//       win.localStorage.setItem(
-//           "profile",
-//           JSON.stringify({ name: "E2E User" })
-//       );
-//   });
-
-//   // Visit the page after "login"
-//   cy.visit("./");
-
-//   // Ensure the logout button is visible
-//   cy.get('button[data-auth="logout"]').should("be.visible");
-
-//   // Click the logout button
-//   cy.get('button[data-auth="logout"]').click();
-
-//   // Ensure the user is redirected to the home page
-//   cy.url().should("eq", "/");
-
-//   // Verify that the token is removed from localStorage
-//   cy.window().then((win) => {
-//       expect(win.localStorage.getItem("token")).to.be.null;
-//   });
-// });
