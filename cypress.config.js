83fa36bdf6e498a2ff518ce53a677347e087f995
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-16 11:50:56 +0200

    Added env in cypress to seperate file and in  gitIgnore

--- cypress.config.js
+++ cypress.config.js
@@ -2,21 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        // Updated path to match your own folder system
+        // Updated the path to match my folder system
         specPattern: "tests/e2e/cypress/specs/**/*.cy.js",
         supportFile: "tests/e2e/cypress/support/commands.js",
         fixturesFolder: "tests/e2e/cypress/fixtures.js",
-        env: {
-            // Add your environment variables here, like user credentials
-            userEmail: "e2euser@noroff.no",
-            userPassword: "e2ePassword",
-            API_BASE_URL: "https://nf-api.onrender.com/api/v1", // Example API URL
-        },
-
-        defaultCommandTimeout: 10000, // Extend command timeout to 10 seconds
+        defaultCommandTimeout: 10000, // Extended command timeout to 10 seconds
 
         setupNodeEvents(/* on, config */) {
-            // implement node event listeners here
         },
     },
 });
--- tests/e2e/cypress/specs/loginFails.cy.js
+++ tests/e2e/cypress/specs/loginFails.cy.js
@@ -4,7 +4,6 @@ describe("Invalid login attempts", () => {
     });
 
     it.skip("should show an error message for invalid login credentials", () => {
-        // Intercept the login API call and mock a 401 response
         cy.intercept(
             "POST",
             `${Cypress.env("API_BASE_URL")}/social/auth/login`,
@@ -25,24 +24,20 @@ describe("Invalid login attempts", () => {
         cy.wait(500);
 
         // Enter invalid email and password
-        cy.get("#loginForm #loginEmail").type("invaliduser@noroff.no");
-        cy.get("#loginForm #loginPassword").type("wrongPassword");
+        cy.get("#loginForm #loginEmail").type(Cypress.env("userEmail")); // valid email from environment variable
+        cy.get("#loginForm #loginPassword").type("wrongPassword"); // invalid password
         cy.wait(500);
 
-        // Stub the window.alert before clicking the submit button
         cy.window().then((win) => {
             cy.stub(win, "alert").as("alertStub");
         });
 
-        // Click on the submit button
         cy.get("#loginForm button[type='submit'].btn-success").click();
 
-        // Wait for the API call and assert the 401 response
         cy.wait("@invalidLoginRequest")
             .its("response.statusCode")
             .should("eq", 401);
 
-        // Assert that the alert is called with the correct message
         cy.get("@alertStub").should("be.calledWith", "Invalid credentials");
     });
 });
