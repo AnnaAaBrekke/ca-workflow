9452c77a5cd10c8fa345ae014206236657c4302b
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-16 13:47:43 +0200

    env secrets in unit-test.yml workflow

--- tests/unit/api/auth.test.js
+++ tests/unit/api/auth.test.js
@@ -2,7 +2,7 @@ const { login } = require("../../../src/js/api/auth/login");
 const { logout } = require("../../../src/js/api/auth/logout");
 const storage = require("../../../src/js/storage");
 
-// Mock the storage module
+// Mocking the storage module
 jest.mock("../../../src/js/storage", () => ({
     save: jest.fn(),
     load: jest.fn(),
@@ -16,7 +16,7 @@ describe("Auth functions", () => {
 
     describe("Login function", () => {
         test("should store token in storage when valid credentials are provided", async () => {
-            // Mock the fetch API to simulate a successful login response
+            // Mocking the fetch API to simulate a successful login response
             global.fetch = jest.fn(() =>
                 Promise.resolve({
                     ok: true,
@@ -28,19 +28,15 @@ describe("Auth functions", () => {
                 })
             );
 
-            // Mock to simulate no token initially in the storage
             storage.load.mockReturnValueOnce(null);
 
-            // Valid credentials
             await login(
                 process.env.JEST_USER_EMAIL,
                 process.env.JEST_USER_PASSWORD
             );
 
-            // Checks that the token was stored correctly
             expect(storage.save).toHaveBeenCalledWith("token", "mock-token");
 
-            // Verify that the fetch function was called with correct arguments
             expect(fetch).toHaveBeenCalledWith(
                 `${process.env.API_BASE_URL}/social/auth/login`,
                 expect.objectContaining({
