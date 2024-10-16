d3f121cee6de0637953822ab3b7f4de86c5c6fb1
AnnaAaBrekke <annabrekke98@gmail.com>
2024-10-16 14:39:49 +0200

    split jest auth  into two individual it bloc ks and added extra wait

--- tests/e2e/cypress/specs/auth.cy.js
+++ tests/e2e/cypress/specs/auth.cy.js
@@ -4,19 +4,26 @@ describe("Login and logout", () => {
     });
 
     it("should log in the user with valid credentials", () => {
-        // Login custom command is to find
+        // Login custom command is to find in /tests/e2e/cypress/support/command.js
         cy.login();
     });
 
-    it.skip("should log out the user and clear the access token", () => {
+    it("should log out the user and clear the access token", () => {
+        cy.wait(500);
+
         cy.login();
+        cy.wait(500);
 
         cy.get("button[data-auth='logout']").click();
 
+        cy.wait(500);
+
         cy.url().should("not.include", "profile");
+        cy.wait(500);
 
         cy.window().then((win) => {
             expect(win.localStorage.getItem("accessToken")).to.be.null;
         });
+        cy.wait(500);
     });
 });
